7e7ea613f9fd37d199fcde3c718b168f17f650bd
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-04-24 20:58:13 -0700

    updated

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,15 +1,21 @@
-import garri from "../images/tropical sun garri.jpg"
-import smallgarri from "../images/Abebi garri.jpg"
-
+import garri from "../images/yellow-Garri.jpg";
+import smallgarri from "../images/Abebi garri.jpg";
 
 const Hero = () => {
   return (
     <div className="hero">
       <input type="text" placeholder="search favourite garri here" />
-          <img src={garri} alt="garri-pics" />
-          <span><img src={ smallgarri } alt="smallgarri" /></span>
+      <img src={garri} alt="garri-pics" />
+      <span>
+        <img src={smallgarri} alt="smallgarri" />
+      </span>
       <h1>Saving Nigerians since 1929</h1>
-      <p>The hunger fighter</p>
+      <p>The hunger destroyer</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, <br/>adipisicing elit. Facilis,
+        nobis! Modi dolores eligendi<br/> nisi impedit, et, hic assumenda dolorum,
+        minima voluptatem odio amet! Nisi.
+      </p>
       <button>GET STARTED</button>
     </div>
   );
