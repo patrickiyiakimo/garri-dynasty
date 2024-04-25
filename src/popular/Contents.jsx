bacdbe923cbb76c6e1fcc49c6c2cb4d8fbbb5395
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-04-24 20:31:35 -0700

    updated

--- src/popular/Contents.jsx
+++ src/popular/Contents.jsx
@@ -1,151 +1,150 @@
 import { useState } from "react";
 import { FaHeart } from "react-icons/fa";
-// import NagikoImage from "../images/Nagiko Garri.jpg"; 
+import NagikoImage from "../images/Nagiko Garri.jpg"; 
 
 
 
 const Contents = () => {
     const [items, setItems] = useState([
       {
-            image: "../images/Nagiko Garri.jpg",
-        //   image: NagikoImage,
-        title: "Nagiko garri",
+      
+        title: "Nagiko Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         image: "../images/Abebi garri.jpg",
         id: 1,
       },
       {
-        image: "../images/soaking garri.jpg",
-        title: "soaking garri",
+      
+        title: "Soaking Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 2,
       },
       {
-        image: "../images/tropical sun garri.jpg",
-        title: "tropical sun garri",
+      
+        title: "Tropical Sun Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 3,
       },
       {
-        image: "../images/Ayosifam garri.jpg",
-        title: "Ayosifam garri",
+  
+        title: "Ayosifam Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 4,
       },
       {
-        image: "../images/Garri- Recipe.webp",
-        title: "Garri-Recipe garri",
+       
+        title: "Garri-Recipe Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 5,
       },
       {
-        image: "../images/yellow-garri.jpg",
-        title: "yellow-garri",
+    
+        title: "Yellow-Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 6,
       },
       {
-        image: "../images/tropical sun garri.jpg",
-        title: "tropical sun garri",
+    
+        title: "Tropical Sun Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 7,
       },
       {
-        image: "../images/Ayosifam garri.jpg",
-        title: "Ayosifam garri",
+        
+        title: "Ayosifam Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 8,
       },
       {
-        image: "../images/nigerian taste garri.jpg",
-        title: "nigerian taste garri",
+        
+        title: "Nigerian Taste Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 9,
       },
       {
-        image: "../images/Abebi garri.jpg",
-        title: "Adebi garri",
+      
+        title: "Adebi Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 10,
       },
       {
-        image: "../images/satchet garri.jpg",
-        title: "satchet garri",
+       
+        title: "Satchet Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 11,
       },
       {
-        image: "../images/tropical sun garri.jpg",
-        title: "tropical garri",
+      
+        title: "Tropical Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 12,
       },
       {
-        image: "../images/Abebi garri.jpg",
-        title: "Adebi garri",
+      
+        title: "Adebi Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 13,
       },
       {
-        image: "../images/Ayosifam garri.jpg",
-        title: "Ayosifam garri",
+       
+        title: "Ayosifam Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 14,
       },
       {
-        image: "../images/cassa garri.jpg",
-        title: "cassa garri",
+       
+        title: "Cassa Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 15,
       },
       {
-        image: "../images/tropical sun garri.jpg",
-        title: "tropical sun garri",
+      
+        title: "Tropical Sun Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 16,
       },
       {
-        image: "../images/mama put garri.jpg",
-        title: "mama put garri",
+       
+        title: "Mama Put Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 17,
       },
       {
-        image: "../images/soaking garri.jpg",
-        title: "soaking garri",
+      
+        title: "Soaking Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 18,
       },
       {
-        image: "../images/tropical sun garri.jpg",
-        title: "tropical sun garri",
+       
+        title: "Tropical Sun Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 19,
       },
       {
-        image: "../images/Abebi garri.jpg",
-        title: "Abebi garri",
+ 
+        title: "Abebi Garri",
         price: "$1000",
-        rating: "4.5",
+        rating: "Rating: 4.5",
         id: 20,
       },
     ]);
@@ -154,11 +153,12 @@ const Contents = () => {
     <div className="content">
       {items.map((item) => (
         <div className="container" key={item.id}>
-          <FaHeart />
-          <img src={item.image} alt={item.title} />
+          <FaHeart className="svg"/>
+          <img src={NagikoImage} alt="nakigo" />
           <p>{item.title}</p>
           <p>{item.price}</p>
           <p>{item.rating}</p>
+          <button>ADD TO CART</button>
         </div>
       ))}
     </div>
