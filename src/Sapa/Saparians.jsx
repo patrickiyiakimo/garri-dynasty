e3d8072846900a578e071801aade635de051c584
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-05-10 12:51:25 +0100

    Update Saparians.jsx

--- src/Sapa/Saparians.jsx
+++ src/Sapa/Saparians.jsx
@@ -1,12 +1,32 @@
+import React from "react";
 import Footer from "./Footer";
 
 const Saparians = () => {
+  const [email, setEmail] = React.useState("");
+
+  const handleEmailChange = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  const subscribe = () => {
+    if (email.trim() === "") {
+      return alert("Please enter your email to subscribe");
+    } else {
+      return alert("Thank you for subscribing");
+    }
+  };
+
   return (
     <div className="sapa">
       <h3>Food for saparians!!!</h3>
-      <input type="text" placeholder="subscribe to our latest garri" />
-          <button onClick={() => alert('Thanks for Subscribing')}>Subscribe</button>
-          <Footer />
+      <input
+        type="text"
+        placeholder="subscribe to our latest garri"
+        onChange={handleEmailChange}
+      />
+      <button onClick={subscribe}>Subscribe</button>
+      <Footer />
     </div>
   );
 };
